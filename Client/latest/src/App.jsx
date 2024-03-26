5aed911b243b904cb5c777f8bc9e9f42c3a32343
unknown <vaishnavi.salunkhe@kalvium.community>
2024-03-26 14:00:15 +0530

    made some changes according to feedback

--- Client/latest/src/App.jsx
+++ Client/latest/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route ,Routes} from 'react-router-dom';
 import Home from "./Components/Home";
 import Login from "./Components/Login";
-import Detail from "./Components/Detail";
 
 function App(){
   return(
@@ -11,7 +10,6 @@ function App(){
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path="/Home" element={<Home/>}/>
-      <Route path="/Detail" element={<Detail/>}/>
     </Routes>
     </BrowserRouter>
     
--- Client/latest/src/Components/Login.jsx
+++ Client/latest/src/Components/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import photo from "../images/geound.jpg";
 import football from "../images/OIP.jpeg";
-import mit from "../images/Mit.jpeg"
+import mitImage from "../images/Mit.jpeg"
 import "./Login.css";
 import { Link } from "react-router-dom";
 
@@ -9,16 +9,16 @@ function Login() {
   return (
     <div>
       <div className="backgroundimg1">
-        <img height={660} width={1260} src={photo} alt="backgroundimg" />
+        <img height={660} width={1260} src={photo} />
       </div>
       <div className="mit">
-        <img className="mitimg" src={mit} alt="mit"/>
+        <img className="mitimg" src={mitImage}/>
       </div>
       <div className="login">
         <h1 className="text0">Login</h1>
       </div>
       <div className="login-container">
-        <img className="football-img" src={football} alt="football" />
+        <img className="football-img" src={football}/>
         <div className="login-box">
           <h2 className="center-text">"Step onto the field of champions with every login."</h2>
           <form>
