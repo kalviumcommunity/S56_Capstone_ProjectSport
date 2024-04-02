5fe34385a2db144001f5acc6d76540c33c9f804e
unknown <vaishnavi.salunkhe@kalvium.community>
2024-04-02 11:05:56 +0530

    Made changes according to feedback

--- Client/latest/src/Pages/contact.jsx
+++ Client/latest/src/Pages/contact.jsx
@@ -20,103 +20,103 @@ function Contact(){
     return(
         <div>
             <Nav/>
-            <div className="main-container">
+            <div className="main">
                 <div>
-                    <h1 className="main-heading">Contact Us...</h1>
+                    <h1 className="heading">Contact Us...</h1>
                 </div>
-                <div className="contact-card">
-                    <div className="card">
+                <div className="contact-box">
+                    <div className="box">
                         <img className="contact-img" src={badminton} alt="badminton" />
                         <h2 className="contact-title">Badminton</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={football} alt="football" />
                         <h2 className="contact-title">Football</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={basketball} alt="basketball" />
                         <h2 className="contact-title">BasketBall</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={cricket} alt="cricket" />
                         <h2 className="contact-title">Cricket</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={swim} alt="swim" />
                         <h2 className="contact-title">Swimming</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={gym} alt="gym" />
                         <h2 className="contact-title">Gym</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={kho} alt="kho-kho" />
                         <h2 className="contact-title">Kho-kho</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={kabaddi} alt="kabaddi" />
                         <h2 className="contact-title">Kabaddi</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={tennis} alt="tennis" />
                         <h2 className="contact-title">Tennis</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                         <img className="contact-img" src={volly} alt="volly" />
                         <h2 className="contact-title">VolleyBall</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                     <img className="contact-img" src={chess} alt="chess" />
                         <h2 className="contact-title">Chess</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                     <img className="contact-img" src={rowing} alt="rowing" />
                         <h2 className="contact-title">Rowing</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                     <img className="contact-img" src={boxing} alt="boxing" />
                         <h2 className="contact-title">Boxing</h2>
                         <h3 className="contact-name">Name : </h3>
                         <h3 className="contact-no">Phone : </h3>
                     </div>
 
-                    <div className="card">
+                    <div className="box">
                     <img className="contact-img" src={arc} alt="archery" />
                         <h2 className="contact-title">Archery</h2>
                         <h3 className="contact-name">Name : </h3>
