df61d898e688ee0e50384e7cc282ac4602dba55e
unknown <vaishnavi.salunkhe@kalvium.community>
2024-04-05 13:22:05 +0530

    Made the different batches

--- Client/latest/src/App.jsx
+++ Client/latest/src/App.jsx
@@ -7,6 +7,7 @@ import Detail from "./Pages/Detail";
 import Contact from "./Pages/contact";
 import Register from "./Pages/Register";
 
+
 function App(){
   return(
     
@@ -18,6 +19,7 @@ function App(){
       <Route path="/Detail" element={<Detail/>}/>
       <Route path="/Contact" element={<Contact/>}/>
       <Route path="/Register" element={<Register/>}/>
+     
 
     </Routes>
     </BrowserRouter>
