28d403d73b22f8317369b4abcbb5adc6a482506a
unknown <vaishnavi.salunkhe@kalvium.community>
2024-04-25 11:42:43 +0530

    Created a card page

--- Client/latest/src/Pages/Login.jsx
+++ Client/latest/src/Pages/Login.jsx
@@ -20,13 +20,36 @@ function Login() {
     });
   };
 
-  const handleSubmit = (e) => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("Submitted Form Data:", formData);
+  //   setFormData({
+  //     username: "",
+  //     password: ""
+  //   });
+  // };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Submitted Form Data:", formData);
-    setFormData({
-      username: "",
-      password: ""
-    });
+    try {
+      const response = await fetch('/api/validate-user', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const isValidUser = await response.json();
+      if (isValidUser) {
+        console.log('Logged in successfully');
+        setFormData({
+          username: "",
+          password: ""
+        });
+      } else {
+        console.log('Invalid username or password');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
   };
 
   return (
