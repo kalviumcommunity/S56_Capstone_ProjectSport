f4d0a1a98a2c40191bbb856a0b9da98908ca9b4a
unknown <vaishnavi.salunkhe@kalvium.community>
2024-05-18 09:47:49 +0530

    changes made in server

--- Server/server.js
+++ Server/server.js
@@ -33,26 +33,26 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post(
-  '/register',
-  [
-      check('Batch').notEmpty().isString().withMessage('Batch is required and must be a string'),
-      check('Sport').notEmpty().isString().withMessage('Sport is required and must be a string'),
-      check('Timing').notEmpty().isString().withMessage('Timing is required and must be a string'),
-  ],
-  async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-      }
+// app.post(
+//   '/register',
+//   [
+//       check('Batch').notEmpty().isString().withMessage('Batch is required and must be a string'),
+//       check('Sport').notEmpty().isString().withMessage('Sport is required and must be a string'),
+//       check('Timing').notEmpty().isString().withMessage('Timing is required and must be a string'),
+//   ],
+//   async (req, res) => {
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//           return res.status(400).json({ errors: errors.array() });
+//       }
 
-      const { Batch, Sport, Timing } = req.body;
-      try {
-          const newMitsport = new Mitsport({ Batch, Sport, Timing });
-          await newMitsport.save();
-          res.status(201).json(newMitsport);
-      } catch (err) {
-          res.status(500).json({ message: err.message });
-      }
-  }
-);
+//       const { Batch, Sport, Timing } = req.body;
+//       try {
+//           const newMitsport = new Mitsport({ Batch, Sport, Timing });
+//           await newMitsport.save();
+//           res.status(201).json(newMitsport);
+//       } catch (err) {
+//           res.status(500).json({ message: err.message });
+//       }
+//   }
+// );
