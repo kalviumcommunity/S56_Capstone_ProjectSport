b50be2777c6a9838787c8ca3fc62942608717a76
unknown <vaishnavi.salunkhe@kalvium.community>
2024-06-23 11:40:00 +0530

    Made change in delete

--- Server/routes.js
+++ Server/routes.js
@@ -39,25 +39,19 @@ router.put(
     }
 );
 
-// router.delete('/deleteuser/:id', async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const user = await Mitsport.findById(id);
-//         if (!user) {
-//             return res.status(404).json({ message: "User not found" });
-//         }
-//         const result = await Mitsport.findByIdAndDelete(id);
-//         res.json(result);
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
-// });
 
-router.delete('/deleteuser/:id',(req,res)=>{
-    const id = req.params.id;
-    Mitsport.findByIdAndDelete({_id:id})
-    .then (result => res.json(result))
-    .catch (err => res.json(err))
-})
+router.delete('/deleteuser/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await Mitsport.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const result = await Mitsport.findByIdAndDelete(id);
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
